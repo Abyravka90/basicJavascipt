ff0d142da6f7f6f1fbf4f008f631ff450a345193
Abyravka90 <asepcahya.nugraha75@gmail.com>
2022-09-29 14:11:44 +0700

    2022-09-29

---
+++ BasicConcept_mathOperator_1.js
@@ -0,0 +1,44 @@
+/*
+    first operation : 
+    addition
+*/
+var a = 25
+var b = 5
+var c = a+b
+console.log(c)
+
+/*
+    second operation : 
+    subtraction
+*/
+var a = 25
+var b = 5
+var c = a-b
+console.log(c)
+
+/*
+    second operation : 
+    multiplication
+*/
+var a = 10
+var b = 10
+var c = a*b
+console.log(c)
+
+/*
+    second operation : 
+    division
+*/
+var a = 20
+var b = 2
+var c = a/b
+console.log(c)
+
+/*
+    second operation : 
+    modulus
+*/
+var a = 56
+var b = 3
+var c = a%b
+console.log(c)
---
+++ BasicConcept_mathOperator_3_incrementDecrement.js
@@ -0,0 +1,30 @@
+console.log('===========INCREMENT++++++++++++++++++++++')
+//post increment
+var a = 0
+var b = 10
+var a = b+=3
+console.log(a)
+console.log(b)
+
+//pre increment
+var a = 0
+var b = 10
+var a = a+=3+b
+console.log(a)
+console.log(b)
+
+console.log('===========DECREMENT++++++++++++++++++++++')
+
+//post increment
+var a = 0
+var b = 10
+var a = b-=3
+console.log(a)
+console.log(b)
+
+//pre increment
+var a = 0
+var b = 10
+var a = a-=3-b
+console.log(a)
+console.log(b)
